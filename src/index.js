21ce0506d334c4dcfe6f5ece926dc7e4f709a42a
greenmall <nyaszelenzelenzelen@gmail.com>
2023-01-30 06:15:16 +0200

    feat:  reverse-int

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse (n) {
-  reverse(123); // 321
-reverse(233); // 332
-reverse(535); // 535
-reverse(95034); // 43059
+  return +(Math.abs(n).toString().split('').reverse().join(''));
 }
